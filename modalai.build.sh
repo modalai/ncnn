99f540c2ddb6e17921df80c358e11eb5fa819b59
cliffwong <cliff.wong@modalai.com>
2025-12-10 01:52:16 -0800

    prep voxl-vulcan fix

--- modalai.build.sh
+++ modalai.build.sh
@@ -1,6 +1,6 @@
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=/opt/cross_toolchain/aarch64-gnu-7.toolchain.cmake -DNCNN_VULKAN=OFF -DNCNN_BUILD_EXAMPLES=ON ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=/opt/cross_toolchain/aarch64-gnu-7.toolchain.cmake -DNCNN_VULKAN=ON -DNCNN_BUILD_EXAMPLES=ON ..
 
 cd build
 make -j 8
